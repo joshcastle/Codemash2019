b71cbe755c78af862f2c7e5ef3a7c72fede2a376
joshcastle <joshcastle13@gmail.com>
2019-01-07 23:14:05 -0500

    Adding first c# program, and gitignore

---
+++ hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+     Console.WriteLine("Hello, World!"); 
+     Console.WriteLine("Today is " + DateTime.Now.DayOfWeek + "!"); 
+    }
+}
